c4f4608d1e7f22ba374b7a0a94860c4dbdc16ad1
Dion Buckley <dion-b@hotmail.com>
2019-10-14 13:41:41 +0100

    Game loop from notes (unresolved external)

--- EntityComponentSystem/main.cpp
+++ EntityComponentSystem/main.cpp
@@ -1,3 +1,24 @@
+#include "HealthComponent.h"
+#include "PositionComponent.h"
+#include "ControlSystem.h"
+#include "RenderSystem.h"
+
 int main() {
+	Entity player;
+	HealthComponent healthComponent; 
+	PositionComponent positionComponent;
+	player.addComponent(healthComponent);
+	player.addComponent(positionComponent);
+
+	ControlSystem controlSystem;
+	RenderSystem renderSystem;
+	controlSystem.addEntity(player);
+	renderSystem.addEntity(player);
+
+	while (true) {
+		controlSystem.update();
+		renderSystem.update();
+	}
+
 	return 0;
 }
